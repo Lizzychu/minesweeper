cde49845eec96e2d1ebaa9a0953d652c7cad0e3c
Lizzy Chu <b08608021@ntu.edu.tw>
2022-12-14 23:22:07 +0800

    commit empty menu on Mac

---
+++ menu-mac.mm
@@ -0,0 +1,26 @@
+#include "sdl_headers.h"
+
+void *menu_create()
+{
+    return NULL;
+}
+
+void menu_destroy(void *menu)
+{
+
+}
+
+int menu_attach(void *menu, SDL_Window *window)
+{
+    return 0;
+}
+
+void menu_detach(void *menu)
+{
+
+}
+
+int menu_get_event_id(void *menu)
+{
+    return 0;
+}
